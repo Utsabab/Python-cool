93364efcb064274396b2ad6c4097ca3b8cafbe1f
Utsab06 <utsab.khakurel@bison.howard.edu>
2017-06-09 13:49:01 -0700

    Added start & end method which finds the index in the text where the word was found

--- search_method.py
+++ search_method.py
@@ -13,14 +13,28 @@ for pattern in patterns:
     else:
         print 'no match'
         
+
 #Using search method to find if a word is in a given text
 import re
 
 pattern = 'match'
 kalajamai = 'Does this text match the pattern?'
 
-
 if re.search(pattern,  kalajamai):
     print 'found a match!'
 else:
     print 'no match'
+
+
+#Using start() & end() method to find the index of the word found in the text
+import re
+
+pattern = 'match'
+text = 'Does this text match the pattern?'
+
+match = re.search(pattern, text)
+
+s = match.start()
+e = match.end()
+
+print 'Found "%s" in "%s" from %d to %d ("%s")' % (match.re.pattern, match.string, s, e, text[s:e])
